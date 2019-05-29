f89d524056cd4188dc2f7d66b3719a3ec88a1129
atulyanarayan <45818585+atulyanarayan@users.noreply.github.com>
2019-05-29 09:45:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-  name = 'istart',         # How you named your package folder (MyLib)
-
-  packages = ['istart'],   # Chose the same as "name"
-
-  version = '0.1',      # Start with a small number and increase it with every change you make
-
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-
-  description = 'Automate and simplify starting data science',   # Give a short description about your library
-
-  author = 'Atulya Narayan',                   # Type in your name
-
-  author_email = 'atulya.iims@gmail.com',      # Type in your E-Mail
-
-  url = 'https://github.com/atulyanarayan/istart',   # Provide either the link to your github or to your website
-
-  #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-
-  keywords = ['automate', 'simplify', 'intelligent data reading'],   # Keywords that define your package best
-
-  install_requires=[            # I get to this in a second
-
-          'pandas',
-
-          'numpy','glob2'
-
-      ],
-
-  classifiers=[
-
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-
-    'Intended Audience :: Developers',      # Define that your audience are developers
-
-    'Topic :: Software Development :: Build Tools',
-
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-
-    'Programming Language :: Python :: 3.4',
-
-    'Programming Language :: Python :: 3.5',
-
-    'Programming Language :: Python :: 3.6',
-
-  ],
-
+setuptools.setup(
+    name="istart",
+    version="0.0.1",
+    author="Atulya Narayan",
+    author_email="atulya.iims@gmail.com",
+    description="Kick start data science package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atulyanarayan/istart",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
